91630a65b4418c0be2d609c4930cdc88769a572c
RachelKolk <rach.m.obrien@gmail.com>
2019-02-04 15:44:38 -0500

    starting work on story uplaod form

--- refugee/src/Views/LoginView.js
+++ refugee/src/Views/LoginView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 import {adminLogIn} from '../store/actions';
 
@@ -16,16 +17,31 @@ class LoginView extends React.Component {
         this.setState({ [e.target.name]: e.target.value});
     }
 
-    submitLogin = () => {
-        this.props.adminLogIn(this.state.login);
-    };
+    // submitLogin = () => {
+    //     this.props.adminLogIn(this.state.login);
+    // };
+    
+
+    handleSubmit = event => {
+        event.preventDefault();
+        const endpoint = `${process.env.REACT_APP_API_URL}/api/login`;
+
+        axios
+            .post(endpoint, this.state)
+            .then(res => {
+                console.log('response', res.data.token)
+                localStorage.setItem('jwt', res.data.token);
+            })
+            .catch(err => console.log(err))
+        }
+    
 
 
     render() {
         return (
             <LoginForm 
                 handleChanges={this.handleChanges}
-                submitLogin={this.submitLogin}
+                submitLogin={this.handleSubmit}
             />
         );
     }
