40a9e9123a3c0c921b0025c35afe69e796d8d8ca
Peter Murphy <peter.francis.murphy@gmail.com>
2019-02-05 15:28:29 -0500

    Built app with redirects for Netlify

---
+++ refugee/build/precache-manifest.bac900828001d8813de4b6bb2f860c0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "df7a40acb40aa4071eab",
+    "url": "/static/css/main.47ef603f.chunk.css"
+  },
+  {
+    "revision": "df7a40acb40aa4071eab",
+    "url": "/static/js/main.df7a40ac.chunk.js"
+  },
+  {
+    "revision": "4fd40fe1b72920b5eaa8",
+    "url": "/static/css/1.c6d17edf.chunk.css"
+  },
+  {
+    "revision": "4fd40fe1b72920b5eaa8",
+    "url": "/static/js/1.4fd40fe1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4322742c4a3af8d23a57503af92ba527",
+    "url": "/index.html"
+  }
+];
