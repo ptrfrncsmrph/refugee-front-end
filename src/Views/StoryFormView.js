e02aac32da7ab59c4db25bb5f6488f9478adc8f1
Peter Murphy <peter.francis.murphy@gmail.com>
2019-02-05 20:27:07 -0500

    Gets Name from query param, code is ugly

--- src/Views/StoryFormView.js
+++ src/Views/StoryFormView.js
@@ -1,51 +1,66 @@
-import React from 'react';
-import {connect} from 'react-redux';
+import React from "react"
+import { connect } from "react-redux"
 
-import {submitStory} from '../store/actions';
+import { submitStory } from "../store/actions"
 
-import StoryForm from '../components/StoryForm/StoryForm';
+import StoryForm from "../components/StoryForm/StoryForm"
 
 class AddNewStory extends React.Component {
-    state= {
-        submission: {
-            id: '',
-            title: '',
-            text: '',
-            approved: false,
-        }
-    };
-
-    handleChange = e => {
-        this.setState({
-            submission: {
-                ...this.state.submission,
-                [e.target.name]: e.target.value
-            }
-        });
-    };
-
-    addStory = () => {
-        this.props.submitStory(this.state.submission);
-    };
-
-    render() {
-        return (
-            <div>
-                <StoryForm 
-                    handleChange={this.handleChange}
-                    submission={this.state.submission}
-                    addStory={this.addStory}
-                />
-
-            </div>
-        )
+  state = {
+    submission: {
+      id: "",
+      title: "",
+      text: "",
+      approved: false
     }
+  }
 
+  handleChange = e => {
+    this.setState({
+      submission: {
+        ...this.state.submission,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  componentDidMount() {
+    const serialize = paramString =>
+      paramString
+        .slice(1)
+        .split(/&/g)
+        .map(e => e.split(/=/))
+        .reduce((acc, [k, v]) => ({ ...acc, [k]: v }), {})
+
+    const { search } = this.props.location
+    search &&
+      this.setState(prevState => ({
+        ...prevState,
+        submission: { ...prevState.submission, title: serialize(search).name }
+      }))
+  }
+
+  addStory = () => {
+    this.props.submitStory(this.state.submission)
+  }
+
+  render() {
+    return (
+      <div>
+        <StoryForm
+          handleChange={this.handleChange}
+          submission={this.state.submission}
+          addStory={this.addStory}
+          country={this.country}
+        />
+      </div>
+    )
+  }
 }
 
-const mapStateToProps = state => ({});
+const mapStateToProps = state => ({ ...state })
 
 export default connect(
-    mapStateToProps,
-    {submitStory}
-)(AddNewStory);
+  mapStateToProps,
+  { submitStory }
+)(AddNewStory)
--- src/components/StoryForm/StoryForm.js
+++ src/components/StoryForm/StoryForm.js
@@ -1,45 +1,43 @@
-import React from 'react';
+import React from "react"
 
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap"
 
 function StoryForm(props) {
+  console.log(props)
 
-    const handleClick = e => {
-        e.preventDefault();
-        props.addStory();
-    }
+  const handleClick = e => {
+    e.preventDefault()
+    props.addStory()
+  }
 
-    return (
-        <div className="StoryForm">
-            <Form>
-                <FormGroup>
-                    <Label for="name-title">Name</Label>
-                    <Input 
-                        type="text" 
-                        name="title" 
-                        placeholder="please feel free to remain anonymous by entering a false name" 
-                        onChange={props.handleChange}
-                        value={props.submission.title}
-                        />
-                </FormGroup>
-            
-                <FormGroup>
-                    <Label for="submission-text">Share your story</Label>
-                    <Input 
-                        type="textarea" 
-                        name="text" 
-                        placeholder="..."
-                        onChange={props.handleChange}
-                        value={props.submission.text}
-                    />
-                </FormGroup>
-                <Button onClick={handleClick}>Submit</Button>
-           
-            </Form>
+  return (
+    <div className="StoryForm">
+      <Form>
+        <FormGroup>
+          <Label for="name-title">Name</Label>
+          <Input
+            type="text"
+            name="title"
+            placeholder="please feel free to remain anonymous by entering a false name"
+            onChange={props.handleChange}
+            value={props.submission.title}
+          />
+        </FormGroup>
 
-        </div>
-    )
-
+        <FormGroup>
+          <Label for="submission-text">Share your story</Label>
+          <Input
+            type="textarea"
+            name="text"
+            placeholder="..."
+            onChange={props.handleChange}
+            value={props.submission.text}
+          />
+        </FormGroup>
+        <Button onClick={handleClick}>Submit</Button>
+      </Form>
+    </div>
+  )
 }
 
-export default StoryForm;
+export default StoryForm
